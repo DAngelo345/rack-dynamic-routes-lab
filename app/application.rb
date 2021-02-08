8bc8134e8ee2e2ba3ed82f036240a9e6820f65f9
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-02-08 15:16:25 -0500

    commit tree 99be601c021c2ef32e13a0b22162347f5edec47b

---
+++ app/application.rb
@@ -0,0 +1,49 @@
+class Application
+
+    # @@items = ["Apples","Carrots","Pears"]
+  
+    def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+  
+      if req.path.match(/items/)
+        # binding.pry
+        item = req.path.split("/items/").last 
+      if things = @@items.find{|s| s.name == item}
+            resp.write things.price
+      elsif 
+        resp.write "Item not found" 
+        resp.status = 400
+      end           
+   
+  
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+  
+      resp.finish
+    end
+  end 
+  # class Application 
+
+#     @@items = ["Apples","Carrots","Pears"]
+
+#     def call(env)
+#         # binding.pry
+#         resp = Rack::Response.new
+#         req = Rack::Request.new(env)
+
+#         if req.path.match(/items/)
+#             @@items.each do |item|
+#                 resp.write item
+#             #   resp.write "#{item.price}\n"
+#             end
+#         else
+#             resp.write "404"
+#         end
+
+#     end
+
+# end
+
